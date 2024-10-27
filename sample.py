6c77361dc1ec932cedc1b93f660d182f0d2dd7a9
GSrisanth <srisanthchowdary@gmail.com>
2024-10-27 16:17:59 +0530

    my file

---
+++ sample.py
@@ -0,0 +1,74 @@
+'''def evenOdd(x):
+    if (x % 2 == 0):
+        print("even")
+    else:
+        print("odd")'''
+
+'''List = []
+List.append(1)  
+List.append(2)
+print(List)
+
+List.insert (3,12)
+List.insert (0,3)
+print(List)'''
+
+# negative indexing is used for printing the last values of the list in the programme
+
+'''List = [3,4,5,5]
+print (List[1])'''
+
+#indexing starts from 0
+
+'''print (List[-2])'''
+
+'''List = [1,2,3,4,5,6,7,8,9,10,11,12]
+List.remove(5)
+List.remove(6)
+print(List)'''
+
+#pop function os used to remove the last item from the list 
+
+'''List.pop() 
+print(List)'''
+
+#Nested loops
+
+'''a = 10
+if (a % 2 ==  0 ):
+    if (a % 5 == 0):
+        print("a is divisible by both 2 and 5")
+if(a == 11):
+    print("a is 11")
+elif(a==10):
+    print("a is 10")
+else:
+    print("a is not present")'''
+
+# While loop
+
+'''i = 0
+while (i<3):
+    i = i+1
+    print('geek')
+
+a = [1,2,3,4]
+while a:
+    print (a.pop())
+
+i = 10
+while (i < 12):
+    i = i+1
+    print(i)
+    break
+else:
+    print("Break the loop")'''
+
+# The else statement will not be executed if there is a break statement in the while loop itself
+
+
+
+
+
+
+
